009bd532701b33a1e2313549872547ec3c5bbae1
edfransou <eduardo.souza.fumcad@despertar.org.br>
2023-06-01 15:40:15 -0300

    alterando

--- Despertar tec/Estudos (Eduardo)/aula 10.0/banco de dados despertar tec.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM db_mercado.tb_cliente;
--- Despertar tec/oficina03.js
+++
@@ -1,8 +0,0 @@
-if(treu){
-    let a =10;
-    console.log(a);
-
-}
-
-//console.log(a) fora do escopo
-//difença entre let e var (diferença de escopo)
